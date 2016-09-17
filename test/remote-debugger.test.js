e9ebffb2756f1293674dec58e08d5519568a48a1
xudafeng <xudafeng@126.com>
2016-09-17 16:10:26 +0800

    tweak project

--- test/remote-debugger.test.js
+++ test/remote-debugger.test.js
@@ -1,22 +1,9 @@
-/* ================================================================
- * remote-debugger by xdf(xudafeng[at]126.com)
- *
- * first created at : Wed Mar 02 2016 17:06:53 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright  xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
 var RemoteDebugger = require('..');
 
 describe('test', function() {
   it('should be ok', function() {
-    RemoteDebugger.should.be.ok;
+    RemoteDebugger.should.be.ok();
   });
 });
